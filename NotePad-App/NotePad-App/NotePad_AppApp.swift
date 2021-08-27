8655c73a220836b6670b912d1b60fa1b2be116b5
Bergan Oudshoorn <Bergan00@gmail.com>
2021-08-27 08:50:43 -0400

    app start

---
+++ NotePad-App/NotePad-App/NotePad_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotePad_AppApp.swift
+//  NotePad-App
+//
+//  Created by Bergan Oudshoorn on 8/27/21.
+//
+
+import SwiftUI
+
+@main
+struct NotePad_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
